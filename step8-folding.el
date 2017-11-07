6d0ddbed781646677776723232bd1d7fe736c134
Brice Maxime Hugues Ozenne <hpl802@sund.root.ku.dk>
2017-11-07 15:32:01 +0100

    restaure folding before first heading

--- step8-folding.el
+++ step8-folding.el
@@ -1,16 +1,16 @@
 ;;; start files with folding 
 (setq org-startup-folded "content")
 
-(use-package folding :ensure t) 
-(folding-mode-add-find-file-hook)
-(add-hook 'LaTeX-mode-hook 'folding-mode) 
-(add-hook 'ess-mode-hook 'folding-mode) 
-(folding-add-to-marks-list 'ess-mode "## {{{ " "## }}}" nil t)
+;; (use-package folding :ensure t) 
+;; (folding-mode-add-find-file-hook)
+;; (add-hook 'LaTeX-mode-hook 'folding-mode) 
+;; (add-hook 'ess-mode-hook 'folding-mode) 
+;; (folding-add-to-marks-list 'ess-mode "## {{{ " "## }}}" nil t)
 
-;; http://orgmode.org/worg/org-tutorials/org-outside-org.html
+;; ;; http://orgmode.org/worg/org-tutorials/org-outside-org.html
 (defvar outline-minor-mode-prefix "\M-#")
-(require 'outorg) ;; necessary for outshine
-(require 'outshine)
+;; ;; (require 'outorg) ;; necessary for outshine
+(use-package outshine :ensure t)
 (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
 (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
 (add-hook 'LaTeX-mode-hook 'outline-minor-mode)
@@ -21,13 +21,13 @@
 (add-hook 'message-mode-hook 'outline-minor-mode)
 
 (use-package outline-magic :ensure t) 
-;; https://stat.ethz.ch/pipermail/ess-help/2007-June/004140.html
-;; (add-hook 'ess-mode-hook
-;; 	  '(lambda ()
-;; 	     (outline-minor-mode)
-;; 	     (setq outline-regexp "### [*]\\{1,8\\} ")
-;; 	     (defun outline-level
-;; 		 (lambda () (interactive) (cond ((looking-at "^### ") 1)((looking-at "^### ") 2)((looking-at "^###") 3) (t 1000)))
+;; ;; https://stat.ethz.ch/pipermail/ess-help/2007-June/004140.html
+;; ;; (add-hook 'ess-mode-hook
+;; ;; 	  '(lambda ()
+;; ;; 	     (outline-minor-mode)
+;; ;; 	     (setq outline-regexp "### [*]\\{1,8\\} ")
+;; ;; 	     (defun outline-level
+;; ;; 		 (lambda () (interactive) (cond ((looking-at "^### ") 1)((looking-at "^### ") 2)((looking-at "^###") 3) (t 1000)))
 
-;; 	       )
-;; 	     ))
+;; ;; 	       )
+;; ;; 	     ))
