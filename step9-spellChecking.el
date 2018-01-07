116cd6da6582a3f78c6e93c26f816a084c771ef7
Brice Ozenne <brice.ozenne@orange.fr>
2018-01-07 13:00:28 +0100

    add automatic latex update using M-j make pdf in a .tex document

--- step9-spellChecking.el
+++ step9-spellChecking.el
@@ -2,7 +2,7 @@
 
 ;;;; enable spell checking on fly
 (dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
+  (add-hook hook (lambda () (flyspell-mode 0))))
 
 
 ;;;; spell checking on demand
@@ -13,7 +13,7 @@
 
 ;; (setq ispell-dictionary-alist
 ;;  '(("english"  
-;;   "[A-Za-z]" "[^A-Za-z]" "[---']" nil ("-B") nil)
+;;   "[A-Za-z]" "[^A-Za-z]" "[---']" nil "-B")
 ;;  ("francais" 
 ;;   "[A-Za-zÀÂÇ-ËÎÏÔÙÛÜàâç-ëîïôùûü]" "[^A-Za-zÀÂÇ-ËÎÏÔÙÛÜàâç-ëîïôùûü]"
 ;;   "[---']" nil ("-n") "~nroff")
@@ -25,6 +25,20 @@
 ;;   "[---'^`\"]" t nil "~nroff")
 ;;  ))
 
+;; original
+;; ((nil "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-B") nil utf-8)
+ ;; ("francais-tex" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_FR") nil utf-8)
+ ;; ("francais7" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_FR") nil utf-8)
+ ;; ("suisse" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH") nil utf-8)
+ ;; ("suisse-sml" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-40") nil utf-8)
+ ;; ("suisse-med" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-60") nil utf-8)
+ ;; ("suisse-lrg" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-80") nil utf-8)
+ ;; ("suisse-80" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-80") nil utf-8)
+ ;; ("suisse-60" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-60") nil utf8)
+ ;; ("suisse-40" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_CH-40") nil utf-8)
+ ;; ("french" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_FR") nil utf-8)
+ ;; ("french-sml" "[[:alpha:]]" "[^[:alpha:]]" "['.-]" t ("-d" "fr_FR-40") nil utf-8)
+ ;; ...)
 
 ;; skip regions with org code or code chunk
 (add-to-list 'ispell-skip-region-alist '(":\\(PROPERTIES\\|LOGBOOK\\):" . ":END:"))
